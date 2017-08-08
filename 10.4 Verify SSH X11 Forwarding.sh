79f182b7080bbe47eb5423ecf6833bb2ee8a8696
AndyWee1234567890 <1502331J@student.tp.edu.sg>
2017-08-08 15:13:57 +0800

    Add files via upload
    Verify SSH X11 Forwarding is no

---
+++ 10.4 Verify SSH X11 Forwarding.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+printf "\e[0m Checking if X11Forwarding is disabled:  \n"
+if grep "^X11Forwarding no" /etc/ssh/sshd_config > /dev/null; then # Grep "X11Forwarding no" to ensure settings is correct and remove the output with "/dev/null"
+        printf "\033[33;32m PASS \n"
+else
+        printf "\033[33;31m FAIL \n"
+fi
