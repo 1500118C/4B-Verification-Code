99a3d7a2c68fd9d1f8869d7e2afd2e5b43d43926
jocelynseow <30354514+jocelynseow@users.noreply.github.com>
2017-08-08 15:02:37 +0800

    Add files via upload
    Verify the SSH Ciphers

---
+++ 10.10 Verify SSH Ciphers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+printf "\e[0m Checking if the Approved Cipers is correct:  \n"
+if grep "^Ciphers aes128-ctr,aes192-ctr,aes256-ctr" /etc/ssh/sshd_config > /dev/null; then # Grep "Ciphers aes128-ctr,aes192-ctr,aes256-ctr" to ensure settings is correct and remove the output with "/dev/null"
+        printf "\033[33;32m PASS \n"
+else
+        printf "\033[33;31m FAIL \n"
+fi
