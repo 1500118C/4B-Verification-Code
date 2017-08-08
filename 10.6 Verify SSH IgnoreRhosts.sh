4d962608525a9b475476a10695164e375811bf5f
AndyWee1234567890 <1502331J@student.tp.edu.sg>
2017-08-08 15:15:25 +0800

    Add files via upload
    Verify SSH IgnoreRhosts is yes

---
+++ 10.6 Verify SSH IgnoreRhosts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+printf "\e[0m Checking if IgnoreRhosts is enabled:  \n"
+if grep "^IgnoreRhosts yes" /etc/ssh/sshd_config > /dev/null; then # Grep "IgnoreRhosts yes" to ensure settings is correct and remove the output with "/dev/null"
+        printf "\033[33;32m PASS \n"
+else
+        printf "\033[33;31m FAIL \n"
+fi
