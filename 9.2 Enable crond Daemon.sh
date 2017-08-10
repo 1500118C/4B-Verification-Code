4b1c0c397ca571554148784b53ec2e9df4b8ff93
jacobchua <jakubchua98@gmail.com>
2017-08-10 13:08:21 +0800

    Update 9.2 Enable crond Daemon.sh

--- 9.2 Enable crond Daemon.sh
+++ 9.2 Enable crond Daemon.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#9.2 Enable crond Daemon
 printf "\e[0m Checking if Cron is enabled \n"
 if systemctl is-enabled crond | grep enabled >/dev/null; then #Use systemctl to check if cron is enabled
 	printf "\033[33;32m PASS \n"
