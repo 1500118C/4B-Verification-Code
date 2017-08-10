56779e8734ba355fb5831e724c174144de100b4b
jacobchua <jakubchua98@gmail.com>
2017-08-10 13:19:43 +0800

    Update 10.7 Verify SSH HostbasedAuthentication.sh

--- 10.7 Verify SSH HostbasedAuthentication.sh
+++ 10.7 Verify SSH HostbasedAuthentication.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#10.7 Verify SSH HostbasedAuthentication
 printf "\e[0m Checking if HostbasedAuthentication is disabled:  \n"
 if grep "^HostbasedAuthentication no" /etc/ssh/sshd_config > /dev/null; then # Grep "HostbasedAuthentication no" to ensure settings is correct and remove the output with "/dev/null"
         printf "\033[33;32m PASS \n" 
