de16def2999dcb9e32dcb740f0f8b4e10a4531c5
jacobchua <jakubchua98@gmail.com>
2017-08-10 13:22:23 +0800

    Update 10 Compilation.sh

--- 10 Compilation.sh
+++ 10 Compilation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#10 Compiled
 printf "\e[0m Checking if the SSH protocol is correct:  \n"
 if grep "^Protocol 2" /etc/ssh/sshd_config > /dev/null; then # Grep "Protocol 2" to ensure settings is correct and remove the output with "/dev/null"
 	printf "\033[33;32m PASS \n"
