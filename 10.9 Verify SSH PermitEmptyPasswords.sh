e8fa9c1facc92605a89d9c9e890ddb0317195ff0
jacobchua <jakubchua98@gmail.com>
2017-08-10 13:20:26 +0800

    Update 10.9 Verify SSH PermitEmptyPasswords.sh

--- 10.9 Verify SSH PermitEmptyPasswords.sh
+++ 10.9 Verify SSH PermitEmptyPasswords.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#10.9 Verify SSH PermitEmptyPasswords
 printf "\e[0m Checking if PermitEmptyPasswords is disabled:  \n"
 if grep "^PermitEmptyPasswords no" /etc/ssh/sshd_config > /dev/null; then # Grep "PermitEmptyPasswords no" to ensure settings is correct and remove the output with "/dev/null"
         printf "\033[33;32m PASS \n"
