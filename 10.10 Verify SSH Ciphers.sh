e0dc241467db70db34c757eeec3ebcb3441e16c6
jacobchua <jakubchua98@gmail.com>
2017-08-10 13:20:44 +0800

    Update 10.10 Verify SSH Ciphers.sh

--- 10.10 Verify SSH Ciphers.sh
+++ 10.10 Verify SSH Ciphers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#10.10 Verify SSH Ciphers
 printf "\e[0m Checking if the Approved Cipers is correct:  \n"
 if grep "^Ciphers aes128-ctr,aes192-ctr,aes256-ctr" /etc/ssh/sshd_config > /dev/null; then # Grep "Ciphers aes128-ctr,aes192-ctr,aes256-ctr" to ensure settings is correct and remove the output with "/dev/null"
         printf "\033[33;32m PASS \n"
