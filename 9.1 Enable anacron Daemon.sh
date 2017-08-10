7fcbcee6d209084c71dc10bcc161ca306e245be7
jacobchua <jakubchua98@gmail.com>
2017-08-10 13:07:49 +0800

    Update 9.1 Enable Anacron Daemon

--- 9.1 Enable anacron Daemon.sh
+++ 9.1 Enable anacron Daemon.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#9.1 Enable anacron Daemon
 printf "Checking if Anacron is enabled \n"
 if rpm -qa | grep -q cronie-anacron; then # List enabled softwares and grep "cronie-anacron" to check if it is enabled
 	printf "\033[33;32m PASS \n"
