c3d9ca17cbc0061302185903d692b81b7e6f4ebf
jacobchua <jakubchua98@gmail.com>
2017-08-10 13:20:05 +0800

    Update 10.8 Verify SSH Root Login.sh

--- 10.8 Verify SSH Root Login.sh
+++ 10.8 Verify SSH Root Login.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#10.8 Verify SSH Root Login
 printf "\e[0m Checking if PermitRootLogin is disabled:  \n"
 if grep "^PermitRootLogin no" /etc/ssh/sshd_config > /dev/null; then # Grep "PermitRootLogin no" to ensure settings is correct and remove the output with "/dev/null"
         printf "\033[33;32m PASS \n"
